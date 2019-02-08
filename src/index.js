b995cb32719329a32d2b51a0820c0878266d3ebe
Dinuka Arseculeratne <dinukaarseculeratne@Dinukas-MacBook-Pro.local>
2019-02-08 16:14:11 +1100

    with routing and lazy loading

--- src/index.js
+++ src/index.js
@@ -5,22 +5,27 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(request => {
-        return request;
-    }, error => {
-        return Promise.reject(error);
-    }
-);
+    console.log(request);
+    // Edit request config
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 axios.interceptors.response.use(response => {
-        return response;
-    }, error => {
-        return Promise.reject(error);
-    }
-);
+    console.log(response);
+    // Edit request config
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
